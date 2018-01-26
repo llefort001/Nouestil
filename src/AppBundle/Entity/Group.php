09be4bc9dcbe756be60631b5b5fee1c16e485d25
Lucas Lefort <negames64300@gmail.com>
2018-01-26 15:38:48 +0100

    bd group refactor to type

--- src/AppBundle/Entity/Group.php
+++ src/AppBundle/Entity/Group.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 /**
  * Group
  *
- * @ORM\Table(name="group")
+ * @ORM\Table(name="type")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\GroupRepository")
  */
 class Group
